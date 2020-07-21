a53390fd1fb419ae169c2e42046ee4e87752c30f
monalizadsg <monalizadasig@gmail.com>
2020-07-21 21:42:26 +0800

    Added new exercise

--- Array/index.js
+++ Array/index.js
@@ -9,3 +9,14 @@ function ArrayFromRange(min, max) {
 }
 
 console.log(ArrayFromRange(1, 4));
+
+//Create a function called includes that takes an array and a search element.
+// if search element exist, return true; otherwise return false.
+
+const numbers = [1, 2, 3, 4];
+function includes(array, searhElement) {
+  for (let num of array) if (num === searhElement) return true;
+  return false;
+}
+
+console.log(includes(numbers, 4));
