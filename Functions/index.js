0ad1a459ea442ad6de6231a972b066c3f519d75e
monalizadsg <monalizadasig@gmail.com>
2020-07-23 14:27:51 +0800

    Added new exercise

--- Functions/index.js
+++ Functions/index.js
@@ -8,3 +8,16 @@ function sum(...items) {
 }
 
 console.log(sum([1, 2, 3, 4]));
+
+// Create a circle object using the object literal syntax.
+// circle.radius = 2
+// circle.area = 20 ; should be read only.
+
+const circle = {
+  radius: 1,
+  get area() {
+    return Math.PI * this.radius * this.radius;
+  },
+};
+
+console.log(circle.area);
